a4242b36abb5f9f8d4fff4ff32dea70d73cc44b8
satishkumar45 <sksatishkumar133@gmail.com>
2021-12-30 16:34:28 +0530

    c shaped commit

---
+++ venom.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+     int i,j,k,l,m;
+    for(i=0;i<6;i++){
+      printf("* ");
+    }
+    printf("\n");
+    for(j=0;j<6;j++)
+    {
+        printf("*\n");
+    }
+    for(k=0;k<12;k++)
+    {
+        printf("* ");
+    }
+      
+       
+} 
