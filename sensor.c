a4f6f093f184ab871cb8089cc99d5b1546a7e9c3
satishkumar45 <sksatishkumar133@gmail.com>
2022-02-04 12:06:57 +0530

    sample analysed driver

---
+++ sensor.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct memory_write *filewrite
+{  
+     .open=dev_open_the_device_to_hadle,
+     .close=dev_close_the_device_to_resume,
+     .write=dev_to_write_device_to_update,
+     .read=dev_to_read_device_to_read,
+   
+};
+static int memory_write_data(struct memory_write *val,0,NULL,semichip)
+{
+     printk(KERN_ALERT"write the data to device \n");
+     return 0;
+}
+static int memory_read_data(struct memory_write *val,semichip)
+{
+    printk(KERN_ALERT"read the device from device \n");
+    return 0;
+}
+static int open_the_device(struct memory_write *val,int cmd,long arg)
+{
+   
+}
