10ea62df8f8ac4c73d641c9556c92aef3f21be3c
satishkumar45 <sksatishkumar133@gmail.com>
2022-02-04 12:31:00 +0530

    removed the space in line 9 and 28

--- sensor.c
+++ sensor.c
@@ -5,8 +5,7 @@ struct memory_write *filewrite
      .open=dev_open_the_device_to_hadle,
      .close=dev_close_the_device_to_resume,
      .write=dev_to_write_device_to_update,
-     .read=dev_to_read_device_to_read,
-   
+     .read=dev_to_read_device_to_read,   
 };
 static int memory_write_data(struct memory_write *val,0,NULL,semichip)
 {
@@ -25,5 +24,4 @@ static int open_the_device(struct memory_write *val,int cmd,long arg)
 static ioctl(struct file *fil,int cmd,long args)
 {
   printk(KERN_ALERT"IOCTL FUNCTIONAL TO READ AND WRITE ANY DEVICE \n");
-
 }
