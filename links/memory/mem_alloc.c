36a01aae9b33813e5e32fc50f26de036b3fa8cb1
satishkumar45 <sksatishkumar133@gmail.com>
2022-03-12 16:42:15 +0530

    mem

---
+++ links/memory/mem_alloc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   printf("enter the things which composed");
+   int a;
+   struct node {
+       int a;
+       struct node base;
+
+   };
+   base=(struct node *)malloc(sizeof(node));
+
+}
