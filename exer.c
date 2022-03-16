3ae30efe85c68eed3fb3a49ff06c221b80c59697
satishkumar45 <sksatishkumar133@gmail.com>
2022-03-16 19:23:35 +0530

    changes done as per analysis
    Signed-off-by: satishkumar45 <sksatishkumar133@gmail.com>

--- exer.c
+++ exer.c
@@ -2,5 +2,6 @@
 int main()
 {
    printf("welcome\n");
+   apply(2);
    return 0;
 }
---
+++ virtualman.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ static unsigned int man_is_waken_to_start_the_day(int time,int start,int stop,int temperature,"man",1,char *name);
+ static unsigned int man_is_slept_to_stop_the_activity(int time,int start,int stop,int temperature,"man",2,char *name);
+ static unsigned int man_is_working_at_the_office_to_produce_knowledge(int depth,"man",0,1,NULL);
+ static unsigned int man_is_creating_the_things(int thinking,char *name,"man",1,0,NULL);
+ static unsigned int best_knowledge_in_the_world("THINKING");
+ static unsigned int best_book_in_the_world("FRIEND");
+ static unsigned int world(int creation,char *beauty_of_nature);
+ static unsigned int love(int unlimited,char *desires);
+ static unsigned int sacrifice(int counts,int reaching);
+ static unsigned int build(int make_a_chance);
+ static unsigned int resources(int values);
+ static unsigned int survival(char *mentioning);
+ static unsigned int situations(int hurdles,char *mapping);
+} 
