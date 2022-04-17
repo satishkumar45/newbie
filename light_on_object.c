bee9bcd2ca1cc527fbbc9aef7ac7c0dfbd5412f5
satishkumar45 <sksatishkumar133@gmail.com>
2022-04-17 15:19:21 +0530

    Torch_device:Light fall on objects automatically     light is falling on the object automatically.     saving the status in the memory for finding things in darkness.     eliminating the risk hazard in darkness.     saving the labour who enter in to drainage holes.     along with light finding smell of dangerous gases exist in the depth.
    Signed-off-by: satishkumar45 <sksatishkumar133@gmail.com>

---
+++ light_on_object.c
@@ -0,0 +1,18 @@
+/* light falls on object by torch automatically. */
+
+#include<stdio.h>
+int main()
+{
+  
+   char sense,int temp;
+   printf("enter sense :\n");
+   if(sense=='y')
+   {
+      printf("light fall on object\n");
+   }
+   else
+   {
+      printf("light does not fall on object \n");
+   }
+
+}
