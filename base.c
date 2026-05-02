03960e0b8d2bc38823a5feb6993079f6b9f8f67a
Satish <sksatishkumar133@gmail.com>
2026-05-02 17:53:24 +0530

    Eject the device
    basically trying to eject the device.
    to get the estimated results.

---
+++ base.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int eject_the_device()
+{
+    char str = "eject the device";
+    int in=0,out=1;
+    if(in==out)
+    {
+	printf("invalid operation\n");
+    }
+}
+int main()
+{
+   int a;
+   eject_the_device();
+   return 0;
+}
